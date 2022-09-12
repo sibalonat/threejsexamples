1cd8b1011a541c1cb9eb71234246fad22d94256f
Marin Nikolli <sibalonat@gmail.com>
2022-09-12 18:37:56 +0200

    .

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import * as THREE from "https://cdnjs.cloudflare.com/ajax/libs/three.js/r132/three.module.js";
+import { ARButton } from " .libs/threejs-r132/examples/jsm/webxr/ARButton.js";
+// import "https://cdnjs.cloudflare.com/ajax/libs/three.js/r132/three.module.js";
 // import * as THREE from "https://cdnjs.cloudflare.com/ajax/libs/three.js/r132/three.module.js";
 // instead of importing threejs, you use it through the library mindar
 // const THREE = window.MINDAR.IMAGE.THREE;
@@ -23,85 +25,103 @@ document.addEventListener(
       //   })
       // }
 
-    //third
+      //third
 
-    const arButton = document.querySelector('#ar-button');
-    const supported = navigator.xr && await navigator.xr.isSessionSupported('immersive-ar');
-    if (!supported) {
-      arButton.textContent = 'Not supported'
-      arButton.disabled = true
-      return
-    }
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera();
-    const renderer = new THREE.WebGLRenderer({alpha: true});
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setPixelRatio(window.devicePixelRatio);
-    document.body.appendChild(renderer.domElement);
+      //custom button three
+      // const arButton = document.querySelector('#ar-button');
+      // const supported = navigator.xr && await navigator.xr.isSessionSupported('immersive-ar');
+      // if (!supported) {
+      //   arButton.textContent = 'Not supported'
+      //   arButton.disabled = true
+      //   return
+      // }
+      const scene = new THREE.Scene();
+      const camera = new THREE.PerspectiveCamera();
+      const renderer = new THREE.WebGLRenderer({ alpha: true });
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      renderer.setPixelRatio(window.devicePixelRatio);
+      document.body.appendChild(renderer.domElement);
 
-    const geometry = new THREE.BoxGeometry(0.06, 0.06, 0.06);
-    const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-    const mesh = new THREE.Mesh(geometry, material);
-    mesh.position.set(0, 0, -0.3);
-    scene.add(mesh);
-    const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
-    scene.add(light);
+      const geometry = new THREE.BoxGeometry(0.06, 0.06, 0.06);
+      const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+      const mesh = new THREE.Mesh(geometry, material);
+      mesh.position.set(0, 0, -0.3);
+      scene.add(mesh);
+      const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+      scene.add(light);
 
-    let currentSession = null;
-    const initialize = async () => {
-      currentSession = await navigator.xr.requestSession('immersive-ar', {
-        optionalFeatures: ['dom-overlay'], domOverlay: {
-          root: document.body
-        } 
-      });
       renderer.xr.enabled = true;
-      renderer.xr.setReferenceSpaceType('local');
-      await renderer.xr.setSession(currentSession);
-
-      arButton.textContent = 'End'
-
       renderer.setAnimationLoop(() => {
-        renderer.render(scene, camera)
-      })
-    } 
-    const end = () => {
+        renderer.render(scene, camera);
+      });
 
-      currentSession.end();
-      renderer.clear();
-      renderer.setAnimationLoop(null);
+      // part 4
+      const arButton = new ARButton.createButton(renderer, {
+        optionalFeatures: ["dom-overlay"],
+        domOverlay: {
+          root: document.body,
+        },
+      });
 
-      arButton.style.display = 'none'
-    }
+      document.body.appendChild(arButton)
 
-    arButton.addEventListener("click", () => {
-      if (currentSession) {
-        end()        
-      } else {
-        initialize()
-      }
-    })
-    
-    //second step  
-    //   const mindarThree = new window.MINDAR.IMAGE.MindARThree({
-    //     container: document.querySelector('#my-ar-container'),
-    //     imageTargetSrc: "./assets/targets/course-banner.mind",
-    //   });
-    //   const { renderer, scene, camera } = mindarThree;
-    //   const geometry = new THREE.PlaneGeometry(1, 1);
-    //   const material = new THREE.MeshBasicMaterial({
-    //     color: 0x0000ff,
-    //     transparent: true,
-    //     opacity: 0.5,
-    //   });
-    //   const plane = new THREE.Mesh(geometry, material);
-    //   const anchor = mindarThree.addAnchor(0);
-    //   anchor.group.add(plane);
+      //custom session manager three
 
-    //   await mindarThree.start();
-    //   renderer.setAnimationLoop(() => {
-    //     renderer.render(scene, camera);
-    //   });
-    // };
+      // let currentSession = null;
+      // const initialize = async () => {
+      //   currentSession = await navigator.xr.requestSession('immersive-ar', {
+      //     optionalFeatures: ['dom-overlay'], domOverlay: {
+      //       root: document.body
+      //     }
+      //   });
+      //   renderer.xr.enabled = true;
+      //   renderer.xr.setReferenceSpaceType('local');
+      //   await renderer.xr.setSession(currentSession);
+
+      //   arButton.textContent = 'End'
+
+      //   renderer.setAnimationLoop(() => {
+      //     renderer.render(scene, camera)
+      //   })
+      // }
+      // const end = () => {
+
+      //   currentSession.end();
+      //   renderer.clear();
+      //   renderer.setAnimationLoop(null);
+
+      //   arButton.style.display = 'none'
+      // }
+
+      // arButton.addEventListener("click", () => {
+      //   if (currentSession) {
+      //     end()
+      //   } else {
+      //     initialize()
+      //   }
+      // })
+
+      //second step
+      //   const mindarThree = new window.MINDAR.IMAGE.MindARThree({
+      //     container: document.querySelector('#my-ar-container'),
+      //     imageTargetSrc: "./assets/targets/course-banner.mind",
+      //   });
+      //   const { renderer, scene, camera } = mindarThree;
+      //   const geometry = new THREE.PlaneGeometry(1, 1);
+      //   const material = new THREE.MeshBasicMaterial({
+      //     color: 0x0000ff,
+      //     transparent: true,
+      //     opacity: 0.5,
+      //   });
+      //   const plane = new THREE.Mesh(geometry, material);
+      //   const anchor = mindarThree.addAnchor(0);
+      //   anchor.group.add(plane);
+
+      //   await mindarThree.start();
+      //   renderer.setAnimationLoop(() => {
+      //     renderer.render(scene, camera);
+      //   });
+      // };
     };
     start();
 
